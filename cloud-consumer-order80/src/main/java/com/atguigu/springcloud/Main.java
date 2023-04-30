e74d30608df133fd31362c37e0106e95d5b2b20e
lijinlong <2693175852@qq.com>
2023-04-30 23:50:52 +0800

    新增consumer模块

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Main.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+/**
+ * @author LiJinLong
+ * @description TODO
+ * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ * @date 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
